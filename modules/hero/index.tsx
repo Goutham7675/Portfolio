c0d743202442a638e46b8c41e2cee6cd6e7321f6
S.S.Goutham Reddy <139247832+Goutham7675@users.noreply.github.com>
2025-08-08 17:28:33 +0530

    fix: vertically center hero text to match original design

--- modules/hero/index.tsx
+++ modules/hero/index.tsx
@@ -55,7 +55,7 @@ const Hero = () => {
 
   return (
     <section
-      className={`relative flex h-screen w-screen flex-col items-center justify-center overflow-hidden pt-24 md:pt-32 ${
+      className={`relative flex h-screen w-screen flex-col items-center justify-center overflow-hidden ${
         theme === "dark" ? "dark" : "light"
       }`}
       id="hero"
