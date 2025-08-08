75f0e82f257b3a90f8bc2612918ed54b41c1024e
S.S.Goutham Reddy <139247832+Goutham7675@users.noreply.github.com>
2025-08-08 15:35:43 +0530

    fix: prepare for Vercel/Netlify deploy and resolve Prettier plugin conflict

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repoName = 'Portfolio'; // Change if your repo name is different
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  swcMinify: true,
   images: {
     unoptimized: true,
   },
