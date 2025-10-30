bb4ed8792f7f26d0af2187bab7ad8c68522e67fc
S. S. Goutham Reddy <139247832+Goutham7675@users.noreply.github.com>
2025-10-30 22:24:47 +0530

    Updated Resume Link

--- modules/hero/index.tsx
+++ modules/hero/index.tsx
@@ -81,7 +81,7 @@ const Hero = () => {
         </button>
         <a
           className="scale-btn"
-          href="https://drive.google.com/file/d/1tDhJqPUuXLWzr2puOjPCFUXWN3Mb3rI3/view?usp=sharing"
+          href="https://drive.google.com/file/d/1jyCjTPwYFU9OjTJEHiYYj69VSCw16aUl/view?usp=sharing"
           target="_blank"
         >
           Resume
