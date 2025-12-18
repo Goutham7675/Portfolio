dba175a2ba2c9f5cfa7618f8f81503a27b9139d5
S. S. Goutham Reddy <139247832+Goutham7675@users.noreply.github.com>
2025-12-18 17:36:46 +0530

    Refactor header text and remove commented code

--- modules/about/components/Header.tsx
+++ modules/about/components/Header.tsx
@@ -18,31 +18,13 @@ const AboutHeader = () => {
           onMouseLeave={setMouseVariant.default}
         >
           <br /> <br />
-          I&apos;m S.S. Goutham Reddy, <br />a Software{" "}
-          <span className="text-gradient">Developer</span> <br /> who{" "}
+          I&apos;m S.S. Goutham Reddy, <br />
+          a Software <span className="text-gradient">Developer</span> <br />
+          who{" "}
           <Delayed waitBeforeShow={2000}>
             <Type />
           </Delayed>
         </motion.h1>
-        {/* /* {/* <div className="mt-12 flex flex-col items-center justify-center">
-          <a
-            href="/pdf/Goutham Reddy Resume.pdf"
-            download
-            className="inline-block rounded-xl bg-gradient-to-r from-blue-500 to-purple-600 px-6 py-3 text-lg font-semibold text-white shadow-lg transition-transform hover:scale-105 hover:from-blue-600 hover:to-purple-700"
-          >
-            Download Resume
-          </a>
-        </div> */} */ */}
-        {/* <div className="mt-12 flex flex-col items-center justify-center">
-          <button
-            type="button"
-            disabled
-            aria-disabled="true"
-            className="inline-block rounded-xl bg-gradient-to-r from-blue-500 to-purple-600 px-6 py-3 text-lg font-semibold text-white shadow-lg transition-transform opacity-50 cursor-not-allowed"
-          >
-            Download Resume
-          </button>
-        </div> */}
       </div>
     </ScrollOpacity>
   );
